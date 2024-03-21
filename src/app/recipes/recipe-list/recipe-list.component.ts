63bcaff7d3c180d35225698b9abcc47c88f2cd50
Andreea <suntandreea@protonmail.com>
2024-03-21 20:40:58 +0200

    Navigation Bar, Recipe Model, Recipe List

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Kimchi', 'Korean fermentation', 'https://upload.wikimedia.org/wikipedia/commons/f/f8/Various_kimchi.jpg'),
+    new Recipe('Japche', 'Korean noodles', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Polish_Korean_Cuisine_and_Culture_Exchanges_Gradmother%E2%80%99s_Recipes_05.jpg/220px-Polish_Korean_Cuisine_and_Culture_Exchanges_Gradmother%E2%80%99s_Recipes_05.jpg')
+  ];
+}
